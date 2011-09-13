0004c5c586f1f9d47d98fddcee900334e560cbef
luis petek <lmpetek@gmail.com>
2011-09-13 18:19:18 -0300

    new version

--- config/routes.rb
+++ config/routes.rb
@@ -1,28 +1,21 @@
 Rails.application.routes.draw do
-#  resources :empresas
-  # resources :facturas, :as => "hefesto_facturas", :controller => 'hefesto/facturas'
-
   scope "/compras" do
-    resources :facturas, :controller => 'hefesto/facturas'
-    resources :notacreditos, :controller => 'hefesto/notacreditos'
-    resources :recibos, :controller => 'hefesto/recibos'
-    resources :facturanotacreditos, :controller => 'hefesto/facturanotacreditos'
-    resources :facturarecibos, :controller => 'hefesto/facturarecibos'
-
     resources :suppliers, :path => "proveedores" do
       member do
         get 'list_accounts', :action => :list_accounts
         get 'cuentacorriente', :action => :cuentacorriente 
       end
-       
+
       resources :facturas, :as => "hefesto_facturas", :controller => 'hefesto/facturas' do
         member do
           get 'print', :action => :imprimir 
         end
       end  
 
-      resources :recibos, :as => "compra_recibos", :controller => 'hefesto/recibos'
-      resources :notacreditos, :as => "compra_notacreditos", :controller => 'hefesto/notacreditos'
-    end  
+      resources :recibos, :as => "hefesto_compra_recibos", :controller => 'hefesto/recibos'
+      resources :notacreditos, :as => "hefesto_compra_notacreditos", :controller => 'hefesto/notacreditos'
+      resources :facturanotacreditos, :as => "hefesto_compra_facturanotacreditos", :controller => 'hefesto/facturanotacreditos'
+      resources :facturarecibos, :as => "hefesto_compra_facturarecibos", :controller => 'hefesto/facturarecibos'
+    end
   end
 end
