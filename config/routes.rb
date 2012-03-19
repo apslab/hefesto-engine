81f7cbfa062ae114755d8e3380d6bb4d91e51738
luis petek <lmpetek@gmail.com>
2012-03-19 16:33:26 -0300

    factura de compras y ctacte de proveedores

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,14 @@
 Rails.application.routes.draw do
-  scope "/compras" do
-    resources :suppliers, :path => "proveedores" do
-      member do
-        get 'list_accounts', :action => :list_accounts
-        get 'cuentacorriente', :action => :cuentacorriente 
-      end
+  resources :suppliers do
+    resources :facturas, :controller => 'hefesto/facturas'
+    resources :recibos,  :controller => 'hefesto/recibos'
+    resources :notacreditos, :controller => 'hefesto/notacreditos'
+    resources :facturanotacreditos,  :controller => 'hefesto/facturanotacreditos'
+    resources :facturarecibos, :controller => 'hefesto/facturarecibos'
 
-      resources :facturas, :as => "hefesto_facturas", :controller => 'hefesto/facturas' do
-        member do
-          get 'print', :action => :imprimir 
-        end
-      end  
-
-      resources :recibos, :as => "hefesto_compra_recibos", :controller => 'hefesto/recibos'
-      resources :notacreditos, :as => "hefesto_compra_notacreditos", :controller => 'hefesto/notacreditos'
-      resources :facturanotacreditos, :as => "hefesto_compra_facturanotacreditos", :controller => 'hefesto/facturanotacreditos'
-      resources :facturarecibos, :as => "hefesto_compra_facturarecibos", :controller => 'hefesto/facturarecibos'
+    member do
+      get 'list_accounts', :action => :list_accounts
+      get 'cuentacorriente', :action => :cuentacorriente 
     end
   end
 end
